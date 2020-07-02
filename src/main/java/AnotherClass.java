da5870bb340a560c740e97a3b9a27ce6d9e088b2
Tarkaiev <tarkaievd@gmail.com>
2020-07-02 16:41:56 +0300

    Also test and practice Here I added one more class and some other things to first class did from another pc

---
+++ src/main/java/AnotherClass.java
@@ -0,0 +1,8 @@
+public class AnotherClass {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int sub(int a, int b){
+        return a-b;
+    }
+}
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello mates!");
+        System.out.println(AnotherClass.sum(1,2));
+        System.out.println(AnotherClass.sub(10,5));
     }
 }
